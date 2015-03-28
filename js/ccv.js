fa8f3f4b668ced1d1129c76a9a19a49e774bf18b
nixhope <darksentinel@gmail.com>
2015-03-29 03:54:15 +1300

    Basic typeahead
    Added typeahead for subject on the search page

--- js/ccv.js
+++ js/ccv.js
@@ -20,3 +20,40 @@ $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
     menu.css({ left:newpos });
 
 });
+
+// typeahead from https://twitter.github.io/typeahead.js/examples/#scrollable-dropdown-menu
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+$('#subject-search .typeahead').typeahead(
+    {
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'SUBJECTS',
+        displayKey: 'value',
+        source: substringMatcher(SUBJECTS)
+    });
